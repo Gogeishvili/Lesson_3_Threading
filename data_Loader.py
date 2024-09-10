835ac314c3fd37af39a53fb4ed4fbb5bd959e75b
Gogeishvili <gio.gogeishvili@gmail.com>
2024-09-10 21:51:41 +0400

    made second loader system

--- data_Loader.py
+++ data_Loader.py
@@ -1,31 +1,38 @@
 import threading
 import requests
+import concurrent.futures
 
 
 class DataLoader:
 
     def __init__(self):
         self.__result = []
+        self.__lock=threading.Lock()
 
     @property
     def result(self):
         return self.__result
 
-    def __fetch_and_save_data(self, api, lock):
+    def __fetch_and_save_data(self, api):
         response = requests.get(api)
         if response.status_code == 200:
-            with lock:
+            with self.__lock:
                 self.__result.append(response.json())
         else:
             print("incorrect data")
 
     def load_data(self, base_api, start_id, end_id):
-        lock = threading.Lock()
         threads = []
         for i in range(start_id, end_id):
             api = f"{base_api}{i}"
-            thread = threading.Thread(target=self.__fetch_and_save_data, args=(api, lock,))
+            thread = threading.Thread(target=self.__fetch_and_save_data, args=(api,))
             threads.append(thread)
             thread.start()
         for thread in threads:
             thread.join()
+
+    def load_data_2(self, base_api, start_id, end_id):
+        with concurrent.futures.ThreadPoolExecutor() as TP:
+            for i in range(start_id, end_id):
+                api = f"{base_api}{i}"
+                TP.submit(self.__fetch_and_save_data, api)
